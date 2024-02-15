6dad6489d19883f85645b6c2f28f7ed70558da8c
gioalada <aallaaddaa24@gmail.com>
2024-02-15 20:13:03 +0400

    second commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,31 @@
+name = "gio"
+#name aris cvladi
+# = aris cvladistvis mnishvnelobis mimniwebeli simbolo
+# "nika" aris cvladistvis mnishvneloba
+
+surname = "aladashvili"
+
+#print(name)
+#print funqcias gadaecema ekranze gamosatani obieqti
+
+name = "gio"   # es aris str tipis cvladi
+age = 13   # es aris int mteli ricxvi
+height = 60.9 # es aris float tipis cvladi (atwiladi)
+#boolean (bool) tipis cvladi
+
+knows_programing = True  #true or false
+is_ugly = False
+
+isUgly = False # javaskriptuli came lcase
+
+
+print(name + " " + surname)
+
+# stringi aris brwyalebishi moqceuli simboloebi
+# print(name + age )
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programing))
