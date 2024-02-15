f7be2d9c7329286d537c79ca1b48aa1bf7133b55
gioalada <aallaaddaa24@gmail.com>
2024-02-15 20:20:34 +0400

    fourth commit

---
+++ day03/homework/lesson3.py
@@ -0,0 +1,177 @@
+NotADir
+ hhfv 
+fbgb 
+fvfdbdab
+
+fb\\bbbbd233241
+
+33345251treft4wfsz
+gethhg  42tg    24`5t3ghn fgr354    wet`2465t`164yrt2hrgf314wyrhbentyrtwhjg3fqer
+c34c14trc1t5y3h2y`gtrhrghfwehbfjwevhfyuWEB;GIBTQEU9OGUwhfg  q3gvrfikrwuhg9orGWEREIY VERIF   Ufr7QGFIUVwriyp7ergipufGWREWIOY7GRFwkvefiqGEUOF6EW67DFVjyefvr   iu23fyi rwbfjlegfiupEGWFIOwbg;lriybfr
+gethergerg
+rger
+hger
+gerh
+q3
+hy5
+yhqet4
+hasattr 4h4wthrthrt
+h t4r 
+hasattrh4
+4
+hasattrrwh
+4 hasattrtrh 
+trh 
+tr 
+return
+
+hasattrhtr
+ hasattr h t4
+   hasattr4h 
+t4rh
+tr hasattrtrh
+tryh tryhtr
+h rthtrhtrhtrhtr
+h 
+th
+ trytr
+hery 
+hasattrryyh
+ 
+ yh
+r hasattrtg
+hasattrher
+hasattr KeyErrorthe
+rdts 
+rh
+returntrh
+returntrh
+tryhtr
+hasattrh
+hasattrhtr
+hasattrth
+yrh
+erh
+Truehtr
+hasattrh
+tyhy
+
+hasattrt
+th
+hrt
+hasattrr h
+rh
+dgh
+rh r
+
+r 
+ 
+ raisethre
+hasattrth 
+rh 
+erh
+tr hasattrh returnh 
+trh
+rt h 
+eth
+tryh trh
+tth 
+er hasattrtrh
+tryhtr
+ hasattrh e
+ rhb rh 
+hasattrh 
+hasattrtr h
+returntrh
+raiseh
+hre
+hre
+hasattrh
+rh
+erth
+trytr
+hrt
+hre
+hasattrh 
+rh
+tryh KeyError hasattrr hasattrerth
+ tryh 
+ rth 
+try raiseth return 
+hasattr
+tryht
+hasattr hrh
+elifyt
+y
+jh
+eth
+ety
+helpyh
+GeneratorExitgh hh try ht 
+  
+   
+    
+    try 
+    hrtfgnfgxmgh
+    matchgxmfhmrsgmshnm hajnmhghghjmg nhjmhfgh
+    nggf
+    gf
+    gnf
+
+    nh
+    fnh
+    fnh
+    nh
+    nh
+    nh
+    nh
+    nh
+    nh
+    nh
+    nh
+    nh
+
+eth
+typeh
+
+typetyh
+
+hry
+hasattrh
+hasattrh
+sgh
+th
+g
+hasattrfh
+gh
+fhh
+fn 
+hasattrn setr dfn defxn
+ EncodingWarning EncodingWarningnf
+FileNotFoundErrornf
+ngf
+nonlocaln
+GeneratorExitgn
+GeneratorExitgn
+GeneratorExitgn
+GeneratorExitgn
+GeneratorExitgn
+ng
+ng
+globalg
+nxf
+FileNotFoundErrordgsh
+zdfgzdfgzdfzd
+hbncg
+ModuleNotFoundErrorzng
+dfhn hgfhndfgnfhn
+gfn
+d
+hnc
+h
+nvxb
+m,
+ggfbn
+fgjm
+finallynchgj,
+zdfgmdgfrjmgchmkxfnj
