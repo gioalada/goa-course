9b728fff0c50614ffb0e4836b4eea526b04811f8
gioalada <aallaaddaa24@gmail.com>
2024-02-15 20:02:47 +0400

    first commit

---
+++ day00/homework/lesson0.py
@@ -0,0 +1,51 @@
+from turtle import*
+
+
+#we want to paint a house
+
+#step 1: draw a square
+width(7)
+color("purple")
+begin_fill()
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+# end of square
+
+#drawig a door
+
+forward(70)
+color("pink")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+exitonclick()
