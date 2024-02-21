baf48f116d0b0be5ef5e54e5f979def5e2a80844
gioalada <aallaaddaa24@gmail.com>
2024-02-21 22:26:42 +0400

    fifth comit

---
+++ day05/homework/lesson5.py
@@ -0,0 +1,12 @@
+print gio                print(gio)
+color(red)               color(("red"))
+forward "200"            forward(200)
+left '90'                left(90)
+right*45*                right(45)
+endfill()                end_fill()
+beginfill()              begin_fill()
+go to(200,200)           goto(200,200)
+goto(200 200)            goto(200,200)
+username                 user_name
+age(13)                  age = 13
+name(gio)                name("gio")
